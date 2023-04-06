11de72cefb1f3c980023d6e80440cff2320ba86a
Arth J Detroja <84059984+arthdetroja01@users.noreply.github.com>
2023-04-06 15:51:52 +0530

    updated urls.py

--- warehouse_management/urls.py
+++ warehouse_management/urls.py
@@ -19,14 +19,16 @@ from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('farmer/', include('farmer.urls')),
-    path('warehouse/', include('warehouse.urls')),    
+    path('warehouse/', include('warehouse.urls')),
     path('', RedirectView.as_view(url='home/home/', permanent=True)),
+    path("update_server/", views.update, name="update"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
