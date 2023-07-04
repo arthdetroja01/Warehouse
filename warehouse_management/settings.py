c7ab5ddec9cd48888c0e1fb0d32aff7ca4ac35fa
Arth J Detroja <84059984+arthdetroja01@users.noreply.github.com>
2023-07-04 23:52:05 +0530

    Update settings.py

--- warehouse_management/settings.py
+++ warehouse_management/settings.py
@@ -26,8 +26,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'daiict.warehouses@gmail.com'
-EMAIL_HOST_PASSWORD = 'earcvwznexwlregm'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Email Details
 # Name: warehouse
